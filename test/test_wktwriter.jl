1ff598967415c48911e8580f96c5d53e7f29ba99
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2025-02-16 16:18:33 +0530

    fix tests

--- test/test_wktwriter.jl
+++ test/test_wktwriter.jl
@@ -1,39 +1,30 @@
 @testset "WKT-CRS 2 Writer" begin
-    crs1 = CRS(name="WGS 84", datum="World Geodetic System 1984", 
-               ellipsoid="WGS 84", ellipsoid_major_axis=6378137, ellipsoid_flattening=298.257223563,
-               prime_meridian="Greenwich", projection="LatLon", parameters=[],
-               epsg=4326)
+    crs1 = LatLon(45.0, 90.0)
+    crs2 = TransverseMercator(500000.0, 4000000.0)
 
-    crs2 = CRS(name="UTM Zone 33N", datum="WGS 84",
-               ellipsoid="WGS 84", ellipsoid_major_axis=6378137, ellipsoid_flattening=298.257223563,
-               prime_meridian="Greenwich", projection="Transverse Mercator",
-               projection_method="Transverse Mercator", 
-               parameters=[(name="Latitude of Origin", value=0.0), 
-                           (name="Central Meridian", value=15.0),
-                           (name="Scale Factor", value=0.9996),
-                           (name="False Easting", value=500000.0),
-                           (name="False Northing", value=0.0)],
-               epsg=32633)
+    wkt_crs1 = wkt(crs1)
+    wkt_crs2 = wkt(crs2)
 
-    @test occursin("PROJCRS", wkt(crs1))  # Ensures OGC WKT2 format is used
-    @test occursin("BASEGEOGCRS", wkt(crs1))
-    @test occursin("DATUM", wkt(crs1))
-    @test occursin("ELLIPSOID", wkt(crs1))
+    @test occursin("PROJCRS", wkt_crs1)
+    @test occursin("BASEGEOGCRS", wkt_crs1)
+    @test occursin("DATUM", wkt_crs1)
+    @test occursin("ELLIPSOID", wkt_crs1)
 
-    @test occursin("PROJCRS", wkt(crs2))
-    @test occursin("CONVERSION", wkt(crs2))
-    @test occursin("METHOD", wkt(crs2))
-    @test occursin("Transverse Mercator", wkt(crs2))
+    @test occursin("PROJCRS", wkt_crs2)
+    @test occursin("CONVERSION", wkt_crs2)
+    @test occursin("METHOD", wkt_crs2)
+    @test occursin("Transverse Mercator", wkt_crs2)
 
-    epsg_wkt_4326 = wkt(crs1)
-    epsg_wkt_32633 = wkt(crs2)
+    @test occursin("ID[\"EPSG\", 4326]", wkt_crs1)
+    @test occursin("ID[\"EPSG\", 32633]", wkt_crs2)
 
-    @test occursin("ID[\"EPSG\", 4326]", epsg_wkt_4326)
-    @test occursin("ID[\"EPSG\", 32633]", epsg_wkt_32633)
+    @test occursin("AXIS[\"Latitude\", NORTH]", wkt_crs1)
+    @test occursin("AXIS[\"Longitude\", EAST]", wkt_crs1)
+    @test occursin("LENGTHUNIT[\"metre\", 1]", wkt_crs2)
 
-    @test occursin("PARAMETER[\"Latitude of Origin\"", wkt(crs2))
-    @test occursin("PARAMETER[\"Central Meridian\"", wkt(crs2))
-    @test occursin("PARAMETER[\"Scale Factor\"", wkt(crs2))
-    @test occursin("PARAMETER[\"False Easting\"", wkt(crs2))
-    @test occursin("PARAMETER[\"False Northing\"", wkt(crs2))
+    @test occursin("PARAMETER[\"Latitude of Origin\", 0.0]", wkt_crs2)
+    @test occursin("PARAMETER[\"Central Meridian\", 15.0]", wkt_crs2)
+    @test occursin("PARAMETER[\"Scale Factor\", 0.9996]", wkt_crs2)
+    @test occursin("PARAMETER[\"False Easting\", 500000.0]", wkt_crs2)
+    @test occursin("PARAMETER[\"False Northing\", 0.0]", wkt_crs2)
 end
